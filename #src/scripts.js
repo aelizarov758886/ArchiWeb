1e2b7bd59c81ea4badb3703b58ce240e27e97fa2
aelizarov758886 <115378296+aelizarov758886@users.noreply.github.com>
2022-10-15 17:11:19 +0300

    Add files via upload

---
+++ #src/scripts.js
@@ -0,0 +1,27 @@
+var prev = document.querySelector(".prev");
+var next = document.querySelector(".next");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+prev.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+next.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
